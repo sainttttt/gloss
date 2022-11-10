ae7363a8431c1d9f3d6d45b31ae769e7b3b00821
Saint <saint@st-mini.local>
2022-11-09 22:14:08 -0500

    dfdfd

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-function setTheme(themeName, size) {
-    localStorage.setItem('theme', themeName);
-    localStorage.setItem('size', size);
-    document.querySelector('body').className = themeName;
-    document.querySelector('body').style = size + "px";
-}
-
 const setFontSize = (size) => { 
-    localStorage.setItem('size', size);
-    console.log('size', size);
     document.querySelector('body').style.fontSize = `${size}px`;
 };
 
-const increaseFont = (amount = 5) => {
+const changeFontSize = (amount = 5) => {
   const size = parseInt(window.getComputedStyle(document.querySelector('body'), null)
-    .getPropertyValue('font-size').replace('px'));
-  setFontSize(size + amount);
+    .getPropertyValue('font-size').replace('px')) + amount;
+  setFontSize(size);
+  localStorage.setItem('fontSize', size);
 }
-
-const decrease = (amount = 5) => {
-  const size = parseInt(window.getComputedStyle(document.querySelector('body'), null)
-    .getPropertyValue('font-size').replace('px'));
-  setFontSize(size - amount);
-}
-
 
 const setFont = (fontName, sizeAdjust) => {
+
   const fontFam = window.getComputedStyle(document.querySelector('body'), null).getPropertyValue('font-family');
   console.log('hehere');
 
@@ -33,33 +19,23 @@ const setFont = (fontName, sizeAdjust) => {
     return;
   }
   document.querySelector('body').style.fontFamily = fontName;
-  increaseFont(sizeAdjust);
-}
-
-
-
-const setPixelFont = () => {
-  const pixelFontName = "xe Dogma";
-  const fontFam = window.getComputedStyle(document.querySelector('body'), null).getPropertyValue('font-family');
-  console.log('hehere');
-
-  if (fontFam.replaceAll('"','')  == pixelFontName) {
-    console.log('here')
-
-    return;
+  localStorage.setItem('fontName', fontName);
+  if (sizeAdjust) {
+    changeFontSize(sizeAdjust);
   }
-  document.querySelector('body').style.fontFamily = pixelFontName;
-  increaseFont(40);
 }
 
 
 function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
 
 r(function(){
-  const theme = localStorage.getItem('theme');
-  const size = localStorage.getItem('size');
-  console.log('foo', theme);
-  if (theme) { 
-    setTheme(theme, size);
+  const fontName = localStorage.getItem('fontName');
+  const size = localStorage.getItem('fontSize');
+
+  if (fontName) { 
+    setFont(fontName, false);
+    setFontSize(size);
   }
+
+  document.querySelector('body').style.visibility = 'visible';
 });
